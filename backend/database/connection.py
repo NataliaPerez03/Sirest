3b38980ac97bf580346ea7ccb82256f36e796033
NataliaPerez03 <126698371+NataliaPerez03@users.noreply.github.com>
2026-05-15 23:59:10 -0500

    feat: configuracion para despliegue en Railway con variables de entorno

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,14 +1,19 @@
 import mysql.connector
 from mysql.connector import Error
+from dotenv import load_dotenv
+import os
+
+# Carga las variables del archivo .env
+load_dotenv()
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host="yamanote.proxy.rlwy.net",
-            user="root",
-            password="PYipyKBUbVAJvNdmLMsnPhoYHoFjJOKD",
-            database="railway",
-            port=36471
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT"))
         )
         return connection
     except Error as e:
